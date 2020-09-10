97e04adb8c61d8ae424d9a095ac22e62e495c42d
Dean West <dean.west@provenmethod.com>
2020-09-10 12:38:03 -0400

    Adding fake DB

---
+++ src/server/db/index.js
@@ -0,0 +1,9 @@
+const F01705150050 = require('./F01705150050.json');
+const F01705150090 = require('./F01705150090.json');
+
+// This is our fancy mock database. In the real world, we'd have an actual database :-P
+// Here, we just use the filenames as keys to their associated documents
+module.exports = {
+  F01705150050,
+  F01705150090,
+};
